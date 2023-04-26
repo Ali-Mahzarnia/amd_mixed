cc6c1540993c5a339e97198ab526628fea069124
Ali-Mahzarnia <69542146+Ali-Mahzarnia@users.noreply.github.com>
2023-04-26 12:55:05 -0400

    Add files via upload

---
+++ new_cond_minmm_strmlines/15.R
@@ -0,0 +1,255 @@
+options(java.parameters = "-Xmx2048m")  
+
+library(xlsx)
+library(lme4)
+library(lmerTest)
+library(multcomp)
+library(rstatix)
+
+
+ROIS_name = " ctx-rh-pericalcarine_right_to_ctx-lh-lingual_left"
+
+
+
+state ='Initial'
+#state ='2Year'
+
+
+
+bundle_match_path = '/Users/ali/Desktop/Mar23/amd_mixed/bundle_analysis/new_cond_minmm_strmlines/Statistics_bundlesizelim_distance_15_affinerigid_non_inclusive_symmetric/'
+ID_matcher = read.csv( paste0(bundle_match_path,state,'_', ROIS_name, '_all_bundle_order.csv' ) )
+
+
+
+
+data_path = paste0(bundle_match_path ,'bundle_stats_withid/')
+
+msr_data_amd  = read.csv( paste0(data_path,state, '_AMD_', ROIS_name  ,'_all_bundle_stats.csv' ) )
+msr_data_amd$AMD = 1 
+
+msr_data_ctrl  = read.csv( paste0(data_path,state, '_Control_', ROIS_name  ,'_all_bundle_stats.csv' ) )
+msr_data_ctrl$AMD = 0
+
+
+
+
+
+
+
+
+
+
+
+msr_data = rbind(msr_data_amd, msr_data_ctrl)
+
+
+
+
+pvals= matrix(NA, dim(ID_matcher)[1], 1)
+
+
+for (bdl_num in 1:dim(ID_matcher)[1]) {
+  
+
+
+
+
+
+#bdl_num =10
+#amd_bdl = ID_matcher[bdl_num,]$Paired.2.YR.AMD
+#ctrl_bdl = ID_matcher[bdl_num,]$Paired.2.YR.Control
+
+  if (state == 'Initial'){
+ amd_bdl = ID_matcher[bdl_num,]$Paired.Initial.AMD
+ ctrl_bdl = ID_matcher[bdl_num,]$Paired.Initial.Control
+}
+
+  if (state == '2Year'){
+    amd_bdl = ID_matcher[bdl_num,]$Paired.2.YR.AMD
+    ctrl_bdl = ID_matcher[bdl_num,]$Paired.2.YR.Control
+  }
+
+
+msr_data_amd_bdl = msr_data_amd[ msr_data_amd $Bundle.ID == amd_bdl, ]
+msr_data_ctrl_bdl = msr_data_ctrl[ msr_data_ctrl $Bundle.ID == ctrl_bdl, ]
+# 
+# table_amd = table(msr_data_amd_bdl$Subject)/50
+# table_amd = as.data.frame(t(table_amd))
+# table_amd$AMD = 1
+# table_ctrl = table(msr_data_ctrl_bdl$Subject)/50
+# 
+
+# table_ctrl$AMD = 0
+# table = rbind(table_amd,table_ctrl )
+# 
+# anova(lm( Freq~AMD     ,data=table, REML = TRUE))
+
+
+msr_data_bdl = rbind(msr_data_amd_bdl, msr_data_ctrl_bdl)
+dim(msr_data_bdl)/50
+table(msr_data_bdl$Subject )/50
+#removing based on number of streamline
+# table = as.data.frame(table(msr_data_bdl$Subject )/50)
+# index_left_1s_qtr = table$Freq > quantile(table$Freq, 0.25)
+# left_1s_qtr_subj = table$Var1[index_left_1s_qtr]
+# msr_data_bdl = msr_data_bdl[msr_data_bdl$Subject %in% left_1s_qtr_subj,]
+####
+
+
+
+lm <- lmer( fa~AMD+ (1|Subject)  + Point.ID   ,data=msr_data_bdl, REML = TRUE)
+
+a=anova(lm)
+a
+a$`Pr(>F)`
+
+# if ( a$`Pr(>F)`[1]<=0.05 ) cat("Bundle Top" , bdl_num   , "th is significant with pval", a$`Pr(>F)`[1]  , " \n" )
+pvals[bdl_num] = a$`Pr(>F)`[1]
+
+}
+
+
+
+
+pval_adj_bnf = p.adjust(pvals, method = "bonferroni")
+pval_adj_fdr = p.adjust(pvals, method = "fdr")
+
+
+for (i in 1:dim(pvals)[1]) {
+  
+  if (!is.na(pval_adj_bnf[i]) ){
+    if ( pval_adj_bnf[i]<=0.05 ) cat("Bundle Top" , i   , "th is bnfrni significant with pval", pval_adj_bnf[i] , " \n" )
+  }
+  if (!is.na(pval_adj_fdr[i]) ){
+    if ( pval_adj_fdr[i]<=0.05 ) cat("Bundle Top" , i   , "th is fdr significant with pval",  pval_adj_fdr[i]  , " \n" )
+    
+  }
+}
+cat( ROIS_name, " AT ", state, ":", "\n")
+
+
+
+
+#state ='Initial'
+state ='2Year'
+
+
+
+bundle_match_path = '/Users/ali/Desktop/Mar23/amd_mixed/bundle_analysis/new_cond_minmm_strmlines/Statistics_bundlesizelim_distance_15_affinerigid_non_inclusive_symmetric/'
+ID_matcher = read.csv( paste0(bundle_match_path,state,'_', ROIS_name, '_all_bundle_order.csv' ) )
+
+
+
+
+data_path = paste0(bundle_match_path ,'bundle_stats_withid/')
+
+msr_data_amd  = read.csv( paste0(data_path,state, '_AMD_', ROIS_name  ,'_all_bundle_stats.csv' ) )
+msr_data_amd$AMD = 1 
+
+msr_data_ctrl  = read.csv( paste0(data_path,state, '_Control_', ROIS_name  ,'_all_bundle_stats.csv' ) )
+msr_data_ctrl$AMD = 0
+
+
+
+
+
+
+
+
+
+
+
+msr_data = rbind(msr_data_amd, msr_data_ctrl)
+
+
+
+
+pvals= matrix(NA, dim(ID_matcher)[1], 1)
+
+
+for (bdl_num in 1:dim(ID_matcher)[1]) {
+  
+  
+  
+  
+  
+  
+  #bdl_num =10
+  #amd_bdl = ID_matcher[bdl_num,]$Paired.2.YR.AMD
+  #ctrl_bdl = ID_matcher[bdl_num,]$Paired.2.YR.Control
+  
+  if (state == 'Initial'){
+    amd_bdl = ID_matcher[bdl_num,]$Paired.Initial.AMD
+    ctrl_bdl = ID_matcher[bdl_num,]$Paired.Initial.Control
+  }
+  
+  if (state == '2Year'){
+    amd_bdl = ID_matcher[bdl_num,]$Paired.2.YR.AMD
+    ctrl_bdl = ID_matcher[bdl_num,]$Paired.2.YR.Control
+  }
+  
+  
+  msr_data_amd_bdl = msr_data_amd[ msr_data_amd $Bundle.ID == amd_bdl, ]
+  msr_data_ctrl_bdl = msr_data_ctrl[ msr_data_ctrl $Bundle.ID == ctrl_bdl, ]
+  # 
+  # table_amd = table(msr_data_amd_bdl$Subject)/50
+  # table_amd = as.data.frame(t(table_amd))
+  # table_amd$AMD = 1
+  # table_ctrl = table(msr_data_ctrl_bdl$Subject)/50
+  # 
+  
+  # table_ctrl$AMD = 0
+  # table = rbind(table_amd,table_ctrl )
+  # 
+  # anova(lm( Freq~AMD     ,data=table, REML = TRUE))
+  
+  
+  msr_data_bdl = rbind(msr_data_amd_bdl, msr_data_ctrl_bdl)
+  dim(msr_data_bdl)/50
+  table(msr_data_bdl$Subject )/50
+  #removing based on number of streamline
+  # table = as.data.frame(table(msr_data_bdl$Subject )/50)
+  # index_left_1s_qtr = table$Freq > quantile(table$Freq, 0.25)
+  # left_1s_qtr_subj = table$Var1[index_left_1s_qtr]
+  # msr_data_bdl = msr_data_bdl[msr_data_bdl$Subject %in% left_1s_qtr_subj,]
+  ####
+  
+  
+  
+  lm <- lmer( fa~AMD+ (1|Subject)  + Point.ID   ,data=msr_data_bdl, REML = TRUE)
+  
+  a=anova(lm)
+  a
+  a$`Pr(>F)`
+  
+  # if ( a$`Pr(>F)`[1]<=0.05 ) cat("Bundle Top" , bdl_num   , "th is significant with pval", a$`Pr(>F)`[1]  , " \n" )
+  pvals[bdl_num] = a$`Pr(>F)`[1]
+  
+}
+
+
+
+
+pval_adj_bnf = p.adjust(pvals, method = "bonferroni")
+pval_adj_fdr = p.adjust(pvals, method = "fdr")
+
+
+for (i in 1:dim(pvals)[1]) {
+  if (!is.na(pval_adj_bnf[i]) ){
+  if ( pval_adj_bnf[i]<=0.05 ) cat("Bundle Top" , i   , "th is bnfrni significant with pval", pval_adj_bnf[i] , " \n" )
+  }
+  if (!is.na(pval_adj_fdr[i]) ){
+    if ( pval_adj_fdr[i]<=0.05 ) cat("Bundle Top" , i   , "th is fdr significant with pval",  pval_adj_fdr[i]  , " \n" )
+    
+  }
+  
+}
+cat( ROIS_name, " AT ", state, ":", "\n")
+
+
+
+
+
+
+
+
