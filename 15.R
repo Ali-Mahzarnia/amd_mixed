10037a9e6e52a14e0fe3608f2931a04cdc2c80ae
Ali-Mahzarnia <69542146+Ali-Mahzarnia@users.noreply.github.com>
2023-04-28 14:56:17 -0400

    Add files via upload

--- 15.R
+++ 15.R
@@ -7,8 +7,8 @@ library(multcomp)
 library(rstatix)
 
 
-ROIS_name = " ctx-rh-pericalcarine_right_to_ctx-lh-lingual_left"
-
+ROIS_name = "ctx-rh-pericalcarine_right_to_ctx-lh-lingual_left"
+Lencond =FALSE
 
 
 state ='Initial'
@@ -46,6 +46,12 @@ msr_data = rbind(msr_data_amd, msr_data_ctrl)
 
 
 pvals= matrix(NA, dim(ID_matcher)[1], 1)
+cohen= pvals*0
+Fvalue= pvals*0
+mean_AMD = pvals*0
+mean_CTRL =  pvals*0
+SE_AMD = pvals*0
+SE_CTRL = pvals*0
 
 
 for (bdl_num in 1:dim(ID_matcher)[1]) {
@@ -96,15 +102,43 @@ table(msr_data_bdl$Subject )/50
 ####
 
 
+if (Lencond ==TRUE) {
+##### lenght analysis
+msr_data_bdl2 = msr_data_bdl %>%group_by(Streamlines.ID)%>% 
+  dplyr::summarise(
+    AMD = max(AMD),
+    Len = min(Length),
+    ID = min(Subject)
+  )
+lm <- lmer( Len~AMD+ (1|ID)     ,data=msr_data_bdl2, REML = TRUE)
+}
+##### lenght analysis
 
-lm <- lmer( fa~AMD+ (1|Subject)  + Point.ID   ,data=msr_data_bdl, REML = TRUE)
+if (Lencond ==FALSE) { lm <- lmer( md~AMD+ (1|Subject)  + Point.ID   ,data=msr_data_bdl, REML = TRUE)}
+
+
+emm = emmeans::emmeans(lm , "AMD"  ) 
+emmmeans = emmeans::eff_size(emm, sigma = sigma(lm), edf = df.residual(lm))
+
+summ_cohen = summary(emmmeans)
+effectsz = summ_cohen$effect.size
+
+
 
 a=anova(lm)
 a
 a$`Pr(>F)`
-
+a$`F value`
 # if ( a$`Pr(>F)`[1]<=0.05 ) cat("Bundle Top" , bdl_num   , "th is significant with pval", a$`Pr(>F)`[1]  , " \n" )
 pvals[bdl_num] = a$`Pr(>F)`[1]
+Fvalue[bdl_num] = a$`F value`[1]
+cohen[bdl_num] = effectsz
+
+emm = summary(emm)
+mean_AMD[bdl_num] = emm[2,2]
+mean_CTRL[bdl_num] = emm[1,2]
+SE_AMD [bdl_num]=emm[2,3]
+SE_CTRL [bdl_num]= emm[1,3]
 
 }
 
@@ -118,11 +152,17 @@ pval_adj_fdr = p.adjust(pvals, method = "fdr")
 for (i in 1:dim(pvals)[1]) {
   
   if (!is.na(pval_adj_bnf[i]) ){
-    if ( pval_adj_bnf[i]<=0.05 ) cat("Bundle Top" , i   , "th is bnfrni significant with pval", pval_adj_bnf[i] , " \n" )
-  }
+    if ( pval_adj_bnf[i]<=0.05 ) 
+    {cat("Bundle Top" , i   , "th is bnfrni significant with pval", pval_adj_bnf[i] , " \n" );
+      cat("Means of AMD and Control with their SE are",mean_AMD[i], "(",SE_AMD[i] ,")"," and " ,mean_CTRL[i], "(",SE_CTRL[i] ,")"  , " \n" ) 
+      cat("F and Cohen are",  Fvalue[i] , "  " , cohen[i] , " \n" ) ;    }
+    }
   if (!is.na(pval_adj_fdr[i]) ){
-    if ( pval_adj_fdr[i]<=0.05 ) cat("Bundle Top" , i   , "th is fdr significant with pval",  pval_adj_fdr[i]  , " \n" )
-    
+    if ( pval_adj_fdr[i]<=0.05 )
+    { cat("Bundle Top" , i   , "th is fdr significant with pval",  pval_adj_fdr[i]  , " \n" ); 
+      cat("Means of AMD and Control with their SE are",mean_AMD[i], "(",SE_AMD[i] ,")"," and " ,mean_CTRL[i], "(",SE_CTRL[i] ,")"  , " \n" ) 
+      cat("F and Cohen are",  Fvalue[i] , "  " , cohen[i] , " \n" ) ;  }
+       
   }
 }
 cat( ROIS_name, " AT ", state, ":", "\n")
@@ -163,9 +203,13 @@ msr_data = rbind(msr_data_amd, msr_data_ctrl)
 
 
 
-
 pvals= matrix(NA, dim(ID_matcher)[1], 1)
-
+cohen= pvals*0
+Fvalue= pvals*0
+mean_AMD = pvals*0
+mean_CTRL =  pvals*0
+SE_AMD = pvals*0
+SE_CTRL = pvals*0
 
 for (bdl_num in 1:dim(ID_matcher)[1]) {
   
@@ -214,16 +258,42 @@ for (bdl_num in 1:dim(ID_matcher)[1]) {
   # msr_data_bdl = msr_data_bdl[msr_data_bdl$Subject %in% left_1s_qtr_subj,]
   ####
   
+  if (Lencond ==TRUE) {
+    ##### lenght analysis
+    msr_data_bdl2 = msr_data_bdl %>%group_by(Streamlines.ID)%>% 
+      dplyr::summarise(
+        AMD = max(AMD),
+        Len = min(Length),
+        ID = min(Subject)
+      )
+    lm <- lmer( Len~AMD+ (1|ID)     ,data=msr_data_bdl2, REML = TRUE)
+  }
+  ##### lenght analysis
+  
+  if (Lencond ==FALSE) { lm <- lmer( md~AMD+ (1|Subject)  + Point.ID   ,data=msr_data_bdl, REML = TRUE)}
+  
+  emm = emmeans::emmeans(lm , "AMD"  ) 
+  emmmeans = emmeans::eff_size(emm, sigma = sigma(lm), edf = df.residual(lm))
+  
+  summ_cohen = summary(emmmeans)
+  effectsz = summ_cohen$effect.size
   
   
-  lm <- lmer( fa~AMD+ (1|Subject)  + Point.ID   ,data=msr_data_bdl, REML = TRUE)
   
   a=anova(lm)
   a
   a$`Pr(>F)`
-  
+  a$`F value`
   # if ( a$`Pr(>F)`[1]<=0.05 ) cat("Bundle Top" , bdl_num   , "th is significant with pval", a$`Pr(>F)`[1]  , " \n" )
   pvals[bdl_num] = a$`Pr(>F)`[1]
+  Fvalue[bdl_num] = a$`F value`[1]
+  cohen[bdl_num] = effectsz
+  
+  emm = summary(emm)
+  mean_AMD[bdl_num] = emm[2,2]
+  mean_CTRL[bdl_num] = emm[1,2]
+  SE_AMD [bdl_num]=emm[2,3]
+  SE_CTRL [bdl_num]= emm[1,3]
   
 }
 
@@ -236,11 +306,19 @@ pval_adj_fdr = p.adjust(pvals, method = "fdr")
 
 for (i in 1:dim(pvals)[1]) {
   if (!is.na(pval_adj_bnf[i]) ){
-  if ( pval_adj_bnf[i]<=0.05 ) cat("Bundle Top" , i   , "th is bnfrni significant with pval", pval_adj_bnf[i] , " \n" )
+  if ( pval_adj_bnf[i]<=0.05 ) 
+  {cat("Bundle Top" , i   , "th is bnfrni significant with pval", pval_adj_bnf[i] , " \n" );
+    cat("Means of AMD and Control with their SE are",mean_AMD[i], "(",SE_AMD[i] ,")"," and " ,mean_CTRL[i], "(",SE_CTRL[i] ,")"  , " \n" ); 
+    cat("F and Cohen are",  Fvalue[i] , "  " , cohen[i] , " \n" )   }
   }
   if (!is.na(pval_adj_fdr[i]) ){
-    if ( pval_adj_fdr[i]<=0.05 ) cat("Bundle Top" , i   , "th is fdr significant with pval",  pval_adj_fdr[i]  , " \n" )
-    
+    if ( pval_adj_fdr[i]<=0.05 ) 
+    {cat("Bundle Top" , i   , "th is fdr significant with pval",  pval_adj_fdr[i]  , " \n" ); 
+      
+      cat("Means of AMD and Control with their SE are",mean_AMD[i], "(",SE_AMD[i] ,")"," and " ,mean_CTRL[i], "(",SE_CTRL[i] ,")"  , " \n" ) ;
+      
+      cat("F and Cohen are",  Fvalue[i] , "  " , cohen[i] , " \n" ) ;   }
+ 
   }
   
 }
